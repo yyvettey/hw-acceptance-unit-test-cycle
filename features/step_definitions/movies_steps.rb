ba7069a7d59acee375febce414ea3f1dc252e754
Ye Yuan <ye.yuan@tamu.edu>
2019-03-08 04:49:28 +0000

    cucumber test completed

--- features/step_definitions/movies_steps.rb
+++ features/step_definitions/movies_steps.rb
@@ -23,3 +23,8 @@ Then /I should see all the movies/ do
     step %{I should see "#{movie.title}"}
   end
 end
+
+Then /^the director of "(.*)" should be "(.*)"$/ do |title_value, director_value|
+  movie = Movie.find_by_title(title_value)
+  expect(movie.director).to eq(director_value)
+end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,11 +15,14 @@ module NavigationHelpers
 
     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
       
-    # when /^the edit page for "(.*)"$/
-    #   edit_movie_path(Movie.find_by_title($1).id)
+    when /^the edit page for "(.*)"$/
+      edit_movie_path(Movie.find_by_title($1).id)
       
-    # when /^the details page for "(.*)"$/
-    #   movie_path(Movie.find_by_title($1).id)   
+    when /^the details page for "(.*)"$/
+      movie_path(Movie.find_by_title($1).id)   
+      
+    when /^the Similar Movies page for "(.*)"$/
+      same_director_path(Movie.find_by_title($1).id)
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
