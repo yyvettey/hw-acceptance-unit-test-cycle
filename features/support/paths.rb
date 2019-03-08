0d8cd0ea1de1d5d7705075da93fd172e63ad62ef
Ye Yuan <ye.yuan@tamu.edu>
2019-03-08 00:06:12 +0000

    Scenario_1: add director to existing movie

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -14,6 +14,12 @@ module NavigationHelpers
     case page_name
 
     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+      
+    # when /^the edit page for "(.*)"$/
+    #   edit_movie_path(Movie.find_by_title($1).id)
+      
+    # when /^the details page for "(.*)"$/
+    #   movie_path(Movie.find_by_title($1).id)   
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
